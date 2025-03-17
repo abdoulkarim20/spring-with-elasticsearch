96cbfb9a7bd264b0d7e025930dce569ff28e326e
akdiallo <akdiallo@gainde2000.sn>
2025-03-17 17:30:23 +0000

    Changement de la version de spring de 23 a 17

--- src/main/java/dev/architecture/springbootelasticsearch/SpringBootElasticsearchApplication.java
+++ src/main/java/dev/architecture/springbootelasticsearch/SpringBootElasticsearchApplication.java
@@ -1,13 +1,15 @@
 package dev.architecture.springbootelasticsearch;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class SpringBootElasticsearchApplication {
 
     public static void main(String[] args) {
-        //log.info("\n------------ Starting Spring Boot Elasticsearch Application ------------");
+        log.info("---- Starting Spring Boot Elasticsearch Application ----");
         SpringApplication.run(SpringBootElasticsearchApplication.class, args);
     }
 
