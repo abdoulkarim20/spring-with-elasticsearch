adbc57dc975a58c255786bac8e9eaf4ce909e320
akdiallo <akdiallo@gainde2000.sn>
2025-03-18 17:08:58 +0000

    Configuration de la connexion et creation du repository de elasticsearch

--- src/main/java/dev/architecture/springbootelasticsearch/buisiness/produit/Produit.java
+++ src/main/java/dev/architecture/springbootelasticsearch/buisiness/produit/Produit.java
@@ -1,27 +1,34 @@
 package dev.architecture.springbootelasticsearch.buisiness.produit;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
 
 import java.time.LocalDateTime;
-@Entity
-@Table(name = "td_produits")
-@NoArgsConstructor
-@AllArgsConstructor
+
+@Document(indexName = "products")
 @Data
 public class Produit {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Field(type = FieldType.Text)
     private String nom;
+    @Field(type = FieldType.Text)
     private String description;
+    @Field(type = FieldType.Double)
     private Double prixUnitaire;
-    private Integer quantiteStock;
+    @Field(type = FieldType.Long)
+    private Long quantiteStock;
+    @Field(type = FieldType.Text)
     private String codeBarre;
+    @Field(type = FieldType.Text)
     private String categorie;
+    @Field(type = FieldType.Date, name = "dateCreation", format = {}, pattern = "uuuu-MM-dd'T'HH:mm:ss")
     private LocalDateTime dateCreation;
+    @Field(type = FieldType.Date, name = "dateModification", format = {}, pattern = "uuuu-MM-dd'T'HH:mm:ss")
     private LocalDateTime dateModification;
+    @Field(type = FieldType.Boolean)
     private Boolean statut;
 }
--- src/main/java/dev/architecture/springbootelasticsearch/buisiness/produit/ProduitRepository.java
+++ src/main/java/dev/architecture/springbootelasticsearch/buisiness/produit/ProduitRepository.java
@@ -1,8 +1,8 @@
 package dev.architecture.springbootelasticsearch.buisiness.produit;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProduitRepository extends JpaRepository<Produit, Long> {
+public interface ProduitRepository extends ElasticsearchRepository<Produit, Long> {
 }
